8d129f79eebe2920ceed65d1b2cf04b5c5cd6c03
Profannyti <vij.raghav16@gmail.com>
2024-05-03 09:59:15 +0530

    First

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
@@ -10,6 +10,7 @@ contract Token {
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed from,
@@ -17,9 +18,15 @@ contract Token {
         uint256 value
     );
 
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
     constructor(
         string memory _name,
-        string memory _symbol, 
+        string memory _symbol,
         uint256 _totalSupply
     ) {
         name = _name;
@@ -28,18 +35,58 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) 
+    function transfer(address _to, uint256 _value)
         public
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
+
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
         require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_from] = balanceOf[_from] - _value;
         balanceOf[_to] = balanceOf[_to] + _value;
 
-        emit Transfer(msg.sender, _to, _value);
-
-        return true;   
+        emit Transfer(_from, _to, _value);
     }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns(bool success)
+    {
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success)
+    {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
